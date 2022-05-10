03d4cf0d22f7e125eb64b436a4d8b6edae3b60a1
TatyPimentaLeal <tatyplummer07@gmail.com>
2022-05-10 20:13:12 -0300

    Estudos das aulas de Java Fundamental do GFT Woman DIO

---
+++ src/loops/Ex2_Nota.java
@@ -0,0 +1,26 @@
+package loops;
+
+import java.util.Scanner;
+
+/*Faça um programa que peça uma nota, entre zero e dez. 
+ * Mostre uma mensagem caso o valor seja inválido e continue pedindo até que o usuário informe um valor válido.
+ * */
+
+public class Ex2_Nota {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner (System.in);
+		
+		int nota;
+		
+		System.out.println("Nota: ");
+		nota = scan.nextInt();
+		
+		while(nota < 0 || nota > 10) {
+			System.out.println("A nota inserida é invalida. Por favor, informe um valor válido.");
+			nota = scan.nextInt();
+		}
+		
+	}
+
+}
---
+++ src/methods/Mensagem.java
@@ -0,0 +1,14 @@
+package methods;
+
+public class Mensagem {
+	
+	public String verifica(int idade) {
+		   if (idade > 18) {
+			     return “Maior de idade”;
+			   } else {
+			     return “Menor de idade”;   
+			   }
+	}
+
+
+}
